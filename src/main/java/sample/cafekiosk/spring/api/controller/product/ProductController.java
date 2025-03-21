26d66885fe510067012a69a07f99ebdd7fa2529b
eunoia73 <soo97703@naver.com>
2025-03-21 10:35:07 +0900

    feat: #16 Validation 도입

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,8 +1,10 @@
 package sample.cafekiosk.spring.api.controller.product;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
@@ -17,7 +19,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public void createProduct(ProductCreateRequest request) {
+    public void createProduct(@Valid @RequestBody ProductCreateRequest request) {
         productService.createProduct(request);
     }
 
